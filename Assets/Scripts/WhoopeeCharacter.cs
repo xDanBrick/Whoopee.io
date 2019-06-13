7778a1f70904645bd9cc8a5ca038935d173acb74
Daniel Boyd <dan_boyd20@hotmail.com>
2019-06-13 13:05:40 +0100

    Addd Character stuff

--- Assets/Scripts/WhoopeeCharacter.cs
+++ Assets/Scripts/WhoopeeCharacter.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class WhoopeeCharacter : MonoBehaviour
 {
+    private Rigidbody2D body;
     // Start is called before the first frame update
     void Start()
     {
-        
+        body = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        body.velocity = transform.up;
+    }
+
+    public void Rotate(float value)
+    {
+        transform.Rotate(0.0f, 0.0f, value);
     }
 }
